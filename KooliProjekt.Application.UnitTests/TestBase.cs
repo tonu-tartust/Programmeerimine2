3d804c0c720a34c8a11447ff54b625c19933da31
Gunnar Peipman <gpeipman@hotmail.com>
2026-01-16 09:53:39 +0200

    lisatud testide projekt

---
+++ KooliProjekt.Application.UnitTests/TestBase.cs
@@ -0,0 +1,47 @@
+﻿using KooliProjekt.Application.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KooliProjekt.Application.UnitTests
+{
+    public abstract class ServiceTestBase : IDisposable
+    {
+        private ApplicationDbContext _dbContext;
+        private bool disposedValue;
+
+        protected ApplicationDbContext DbContext
+        {
+            get
+            {
+                if (_dbContext != null)
+                {
+                    return _dbContext;
+                }
+
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+                _dbContext = new ApplicationDbContext(options);
+                return _dbContext;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _dbContext?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
